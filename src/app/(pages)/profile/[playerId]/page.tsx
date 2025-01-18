c4f31ee49287f7b97415c3775e545209b3206b88
Houssine Agha <hosenalrohe@gmail.com>
2025-01-18 13:57:12 +0300

    add courses in profile show

--- src/app/(pages)/profile/[playerId]/page.tsx
+++ src/app/(pages)/profile/[playerId]/page.tsx
@@ -2,8 +2,8 @@ import { Fragment } from "react";
 import Header from "@/section/Header/v2/Header";
 import PageHeader from "@/components/pageHeader/PageHeader";
 import ProfileDetails from "@/section/Profile/ProfileDetails";
-import { getUserProfile } from "@/apiService";
-import { UserProfileData } from "@/interfaces/api";
+import { getProfileCourses, getUserProfile } from "@/apiService";
+import { CoursesResponse, UserProfileData } from "@/interfaces/api";
 
 interface ProfilePageProps {
   params: Promise<{ playerId: string }>;
@@ -12,6 +12,7 @@ interface ProfilePageProps {
 export default async function ProfilePage({ params }: ProfilePageProps) {
   const { playerId } = await params;
   const data: UserProfileData = await getUserProfile(playerId);
+  const courses : CoursesResponse[] = await getProfileCourses(playerId);
 
   return (
     <Fragment>
@@ -19,7 +20,8 @@ export default async function ProfilePage({ params }: ProfilePageProps) {
         <PageHeader currentPage={"Player"} pageTitle={"Player Details"} />
         <ProfileDetails
           playerId={playerId}
-          data={data} /*balance={balanceOfUser}*/
+          data={data} 
+          courses={courses}
         />
     </Fragment>
   );
