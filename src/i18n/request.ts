45bb499a00608f596a10b5188810ec5a228e9886
fade <fadesarakpe12345@gmal.com>
2025-01-29 00:31:19 +0200

    Add And Configuration Localization

---
+++ src/i18n/request.ts
@@ -0,0 +1,20 @@
+import { getCookie } from "cookies-next";
+import { getRequestConfig } from "next-intl/server";
+
+export default getRequestConfig(async () => {
+  // supported languages
+  const supportedLanguages = ["en", "ar"];
+
+  // get language from cookies
+  let selectedLang: any = getCookie("language");
+
+  // if language is not supported, set default language en
+  if (!supportedLanguages.includes(selectedLang)) {
+    selectedLang = "ar";
+  }
+
+  return {
+    locale: selectedLang,
+    messages: (await import(`../../messages/${selectedLang}.json`)).default,
+  };
+});
