209aaca68f64147f53346ebb3af2aa0988965e2c
fade <fadesarakpe12345@gmal.com>
2025-03-04 12:52:30 +0200

    Optimize Search In Home PAge And Fix Error Design In Total Rating

--- src/section/Home/Courses/CoursesList.tsx
+++ src/section/Home/Courses/CoursesList.tsx
@@ -1,35 +1,21 @@
 "use client";
-import React, {  useRef, useState } from "react";
+import React, { useRef } from "react";
 import CourseCard from "./CourseCard/CourseCard";
 import styles from "./CoursesList.module.css";
 import { FiSearch } from "react-icons/fi";
 
 import { useTranslations } from "next-intl";
 import { CoursesResponse } from "@/interfaces/api";
-import { searchOnCourses } from "@/apiService";
+import { useHomeSearch } from "@/hooks/useHomeSearch";
 
 interface CourseList {
-  courses:CoursesResponse[],
+  initCourses:CoursesResponse[],
 }
-export default function CoursesList({courses}: CourseList) {
+export default function CoursesList({initCourses}: CourseList) {
 
   const searchRef = useRef<HTMLInputElement>(null);
   const translate = useTranslations("Home");
-  const [filterCourses, setFilterCourses] = useState<CoursesResponse[]>(courses);
-  
-  
-  
-  const handleSearch = async () => {
-    if (searchRef.current?.value.trim() !== "") {
-      try {
-        const response = await searchOnCourses(searchRef.current!.value);
-        const coursesData: CoursesResponse[] = response;
-        setFilterCourses(coursesData);
-      } catch {}
-    } else {
-      setFilterCourses(courses);
-    }
-  };
+  const { handleSearch, filterCourses } = useHomeSearch(searchRef, initCourses);
 
   return (
     <div className={styles.courseWrapper}>
@@ -53,25 +39,13 @@ export default function CoursesList({courses}: CourseList) {
                 <FiSearch />
               </button>
             </div>
-            {/* <div className="btn" onClick={() => {}}>
-              Newest
-            </div>
-            <div className="btn" onClick={() => {}}>
-              Biggest Points
-            </div>
-            <div className="btn" onClick={() => {}}>
-              My Courses
-            </div>
-            <div className="btn" onClick={() => {}}>
-              All Courses
-            </div> */}
           </div>
         </div>
         <div className="row">
           {filterCourses &&
             filterCourses.map((filteredCourse, i) => (
               <div key={i} className="col-lg-4 col-md-6">
-                <CourseCard props={filteredCourse}/>
+                <CourseCard props={filteredCourse} />
               </div>
             ))}
         </div>
